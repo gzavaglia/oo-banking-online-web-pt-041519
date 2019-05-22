25d684f84071ad2f873b4e79aea0552911d6748f
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-22 01:48:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 class Transfer
-  attr_accessor :sender, :receiver, :status, :transfer_amount
+  attr_accessor :sender, :receiver, :status, :amount
   def initialize(sender, receiver, transfer_amount)
     @sender = sender
     @receiver = receiver
-    @transfer_amount = transfer_amount
+    @amount = transfer_amount
     @status = 'pending'
   end
 end
