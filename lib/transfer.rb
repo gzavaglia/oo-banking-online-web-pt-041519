80c2e1303925ad666560a846d8dd2cf3c0e05560
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-22 01:47:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :transfer_amount
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @transfer_amount = transfer_amount
+    @status = 'open'
+  end
 end
