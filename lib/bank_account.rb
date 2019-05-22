8b4c1ca0a97e3f25677f5bd2a1c1c5992b3b625e
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-22 01:44:51 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,4 +1,10 @@
 class BankAccount
-  attr_accessor :name
-  attr_accessor
-end
+  attr_reader :name
+  attr_accessor :balance, :status
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+end #end class
